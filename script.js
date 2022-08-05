2e7f0726bfac14b871f86ecab41f540f71b9e7f1
Jack Thacker <jackthacker89@gmail.com>
2022-08-05 14:16:36 +0100

    Installed jest to project

---
+++ script.js
@@ -0,0 +1,14 @@
+// TRANSLATABLE LIBRARY CLASS
+// Should be able to take two library parameters and translater one into the other
+// Should be able to take any libraries of equivalents
+
+// MORSE CODE TO ENGLISH CLASS
+// Should be able to handle the two libraries required to translate from MC to Eng
+// Should format the translation so that it is easy to understand
+
+// ENGLISH TO MORSE CODE CLASS
+// Should be able to handle the two libraries required to translate from Eng to MC
+// Should format the translation so that it is easy to understand
+
+// MORSE CODE TRANSLATOR OBJECTS
+// Objects should be able to handle the translations either way (two objects - one for each direction of translation)
