fc0f9d386a0129e7ac78c99d77753fdcbf36b088
Jack Thacker <jackthacker89@gmail.com>
2022-08-08 12:53:54 +0100

    Made a version of the default translator that takes two equivalent arrays instead of an object

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,72 @@
-import { morseCodeAlphabet } from "./alphabet.js";
+import * as alphabet from "./alphabet.js";
+alphabet.englishAlphabet;
+alphabet.morseCodeAlphabet;
 
 // TRANSLATOR CLASS
 // Should be able to take two library parameters and translater one into the other
 // Should be able to take any libraries of equivalents
+
+// OBJECT VERSION
+// export class Translator {
+//   constructor(alphabetArr) {
+//     this.alphabetArr = alphabetArr;
+//   }
+
+//   translateCharacter(singleCharacterStr) {
+//     if (typeof singleCharacterStr === "string") {
+//       let output;
+//       this.alphabetArr.forEach((equivalent) => {
+//         for (let key in equivalent) {
+//           if (key.toUpperCase() === singleCharacterStr.toUpperCase()) {
+//             output = equivalent[key].toString();
+//           }
+//         }
+//       });
+//       return output;
+//     } else {
+//       return "You must enter a string";
+//     }
+//   }
+// }
+
+// const test = new Translator(morseCodeAlphabet);
+
+// console.log(test.translateCharacter("a"));
+
+// ARRAYS VERSION
 export class Translator {
-  constructor(alphabetArr) {
-    this.alphabetArr = alphabetArr;
+  constructor(firstAlphabetArr, secondAlphabetArr) {
+    this.firstAlphabetArr = firstAlphabetArr;
+    this.secondAlphabetArr = secondAlphabetArr;
   }
 
   translateCharacter(singleCharacterStr) {
-    if (typeof singleCharacterStr === "string") {
+    if (
+      typeof singleCharacterStr === "string" &&
+      singleCharacterStr.length === 1
+    ) {
       let output;
-      this.alphabetArr.forEach((equivalent) => {
-        for (let key in equivalent) {
-          if (key.toUpperCase() === singleCharacterStr.toUpperCase()) {
-            output = equivalent[key].toString();
-          }
+      for (let i = 0; i < this.firstAlphabetArr.length; i++) {
+        if (
+          singleCharacterStr.toUpperCase() ===
+          this.firstAlphabetArr[i].toUpperCase()
+        ) {
+          output = this.secondAlphabetArr[i];
         }
-      });
+      }
       return output;
     } else {
-      return "You must enter a string";
+      console.log("You must enter a single character as a string");
     }
   }
 }
 
-const test = new Translator(morseCodeAlphabet);
+const test = new Translator(
+  alphabet.englishAlphabet,
+  alphabet.morseCodeAlphabet
+);
 
-console.log(test.translateCharacter("a"));
+console.log(test.translateCharacter(" "));
 
 // MORSE CODE TO ENGLISH CLASS
 // Should be able to handle the two libraries required to translate from MC to Eng
