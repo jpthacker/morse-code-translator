1b790e6faaf93b74ea2218ca1e203dfe2547beae
Jack Thacker <jackthacker89@gmail.com>
2022-08-08 17:33:02 +0100

    Reconfigured object-based version of translator to handle multiple characters

--- scripts/main.js
+++ scripts/main.js
@@ -1,76 +1,86 @@
 import * as alphabet from "./alphabet.js";
-alphabet.englishAlphabet;
 alphabet.morseCodeAlphabet;
+alphabet.englishAlphabet;
+alphabet.morseCodeAlphabetArr;
 
 // TRANSLATOR CLASS
-// Should be able to take two library parameters and translater one into the other
-// Should be able to take any libraries of equivalents
 
-// OBJECT VERSION
-// export class Translator {
-//   constructor(alphabetArr) {
-//     this.alphabetArr = alphabetArr;
-//   }
-
-//   translateCharacter(singleCharacterStr) {
-//     if (typeof singleCharacterStr === "string") {
-//       let output;
-//       this.alphabetArr.forEach((equivalent) => {
-//         for (let key in equivalent) {
-//           if (key.toUpperCase() === singleCharacterStr.toUpperCase()) {
-//             output = equivalent[key].toString();
-//           }
-//         }
-//       });
-//       return output;
-//     } else {
-//       return "You must enter a string";
-//     }
-//   }
-// }
-
-// const test = new Translator(morseCodeAlphabet);
-
-// console.log(test.translateCharacter("a"));
-
-// ARRAYS VERSION
+// OBJECT-BASED VERSION
 export class Translator {
-  constructor(firstAlphabetArr, secondAlphabetArr) {
-    this.firstAlphabetArr = firstAlphabetArr;
-    this.secondAlphabetArr = secondAlphabetArr;
+  constructor(alphabetArr) {
+    this.alphabetArr = alphabetArr;
   }
 
-  translateCharacter(singleCharacterStr) {
-    if (
-      typeof singleCharacterStr === "string" ||
-      singleCharacterStr.length === 1
-    ) {
-      let output;
-      for (let i = 0; i < this.firstAlphabetArr.length; i++) {
-        if (
-          singleCharacterStr.toUpperCase() ===
-          this.firstAlphabetArr[i].toUpperCase()
-        ) {
-          output = this.secondAlphabetArr[i];
-        }
-      }
-      return output.toString();
+  translateCharacter(str) {
+    if (typeof str === "string") {
+      let output = [];
+      const strArr = str.split("");
+      strArr.forEach((character) => {
+        this.alphabetArr.forEach((equivalent) => {
+          for (let key in equivalent) {
+            if (key.toUpperCase() === character.toUpperCase()) {
+              output.push(equivalent[key].toString());
+            }
+          }
+        });
+      });
+      return output.join(" ");
     } else {
-      return "You must enter a single character as a string";
+      return "You must enter a string";
     }
   }
 }
 
-const test = new Translator(
-  alphabet.englishAlphabet,
-  alphabet.morseCodeAlphabet
-);
+const test = new Translator(alphabet.morseCodeAlphabet);
 
-console.log(test.translateCharacter("a"));
+console.log(test.translateCharacter("Jack Thacker"));
+
+// ARRAYS-BASED VERSION
+// export class Translator {
+//   constructor(firstAlphabetArr, secondAlphabetArr) {
+//     this.firstAlphabetArr = firstAlphabetArr;
+//     this.secondAlphabetArr = secondAlphabetArr;
+//   }
+
+//   translateCharacter(singleCharacterStr) {
+//     if (
+//       typeof singleCharacterStr === "string" ||
+//       singleCharacterStr.length === 1
+//     ) {
+//       let output;
+//       for (let i = 0; i < this.firstAlphabetArr.length; i++) {
+//         if (
+//           singleCharacterStr.toUpperCase() ===
+//           this.firstAlphabetArr[i].toUpperCase()
+//         ) {
+//           output = this.secondAlphabetArr[i];
+//         }
+//       }
+//       return output.toString();
+//     } else {
+//       return "You must enter a single character as a string";
+//     }
+//   }
+// }
+
+// const test = new Translator(
+//   alphabet.englishAlphabet,
+//   alphabet.morseCodeAlphabetArr
+// );
+
+// console.log(test.translateCharacter("a"));
 
 // MORSE CODE TO ENGLISH CLASS
 // Should be able to handle the two libraries required to translate from MC to Eng
 // Should format the translation so that it is easy to understand
+// export class englishToMorseCodeTranslator extends Translator {
+//   constructor(firstAlphabetArr, secondAlphabetArr) {
+//     super(firstAlphabetArr, secondAlphabetArr);
+//   }
+//   translateMorseCode(singleCharacterStr) {
+//     return `${super.translateCharacter(singleCharacterStr)} `;
+//   }
+// }
 
 // ENGLISH TO MORSE CODE CLASS
 // Should be able to handle the two libraries required to translate from Eng to MC
--- scripts/main.test.js
+++ scripts/main.test.js
@@ -1,13 +1,15 @@
-import { Translator } from "./main";
+import * as classes from "./main";
+classes.Translator;
+classes.englishToMorseCodeTranslator;
 
 describe("Translator tests", () => {
   describe("Default translator", () => {
-    // const alphabetArr = [{ A: 1 }, { b: 2 }, { c: "3" }, { 6: 4 }, { ".": 0 }];
-    const engAlphabet = ["a", "b", "c", "d", "e", "1", " "];
-    const morseCodeAlphabet = ["1", "2", 3, "4", "5", "6", " "];
-    const sampleTranslator = new Translator(engAlphabet, morseCodeAlphabet);
+    const alphabetArr = [{ A: 1 }, { b: 2 }, { c: "3" }, { 6: 4 }, { ".": 0 }];
+    // const engAlphabet = ["a", "b", "c", "d", "e", "1", " "];
+    // const morseCodeAlphabet = ["1", "2", 3, "4", "5", "6", " "];
+    const sampleTranslator = new classes.Translator(alphabetArr);
     test("Should be able to take an equivalent alphabet object and translate the key into the value", () => {
-      expect(sampleTranslator.translateCharacter("a")).toBe("1");
+      expect(sampleTranslator.translateCharacter("abc")).toBe("123");
     });
 
     test("Should be able to handle lower and upper case entries", () => {
@@ -20,8 +22,21 @@ describe("Translator tests", () => {
 
     test("Should be able to handle non string entries", () => {
       expect(sampleTranslator.translateCharacter(6)).toBe(
-        "You must enter a single character as a string"
+        "You must enter a string"
       );
     });
   });
+
+  // describe("English to Morse Code Translator", () => {
+  //   const engToMC = new classes.englishToMorseCodeTranslator(
+  //     ["A", "B", "C", "D", "E", "F", " "],
+  //     [".-", "-...", "-.-.", "-..", ".", "..-.", " "]
+  //   );
+  //   test("Should be able to handle the two libraries required to translate from Eng to MC", () => {
+  //     expect(engToMC.translateMorseCode("a")).toBe(".- ");
+  //     expect(engToMC.translateMorseCode("f")).toBe("..-. ");
+  //   });
+
+  //   test("Should format the translation so that it is easy to understand", () => {});
+  // });
 });
